16fabf51e0a248ad6ed2a9846b1e08ddb95db645
Anslie <brantae@gmail.com>
2022-10-25 20:12:31 -0500

    commit tree d23d0dac760498acc023e196073b3fc98aebd78a

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+function EyesOnMe (){
+    function focusHandle() {
+        console.log('Good!')
+    }
+    function blurHandle() {
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button 
+            name="eyes" 
+            onFocus={focusHandle}
+            onBlur={blurHandle}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
 // Code Keypad Component Here
 
-function Keypad (){
+
+
+function Keypad(){
+    
+    function handleChange(event) {
+        event.preventDefault()
+        console.log("Entering password...")
+    }
+    
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password" 
+            name="password" 
+            placeholder="Enter password..."
+            onChange = {handleChange}/>
+        </div>
     )
+    
 }
 
 export default Keypad;
